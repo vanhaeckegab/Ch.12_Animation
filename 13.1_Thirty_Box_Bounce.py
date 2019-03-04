c6a7966bd86d3a99abc05fda828c3c860ad167c8
Marc Hermon <hermonm@urbandaleschools.com>
2019-03-03 19:32:53 -0600

    Update and rename 13.1_Ten_Box_Bounce.py to 13.1_Thirty_Box_Bounce.py

---
+++ 13.1_Thirty_Box_Bounce.py
@@ -0,0 +1,16 @@
+'''
+30 BOX BOUNCE PROGRAM
+--------------------
+
+Modify the Ball Bounce Program to create the following:
+1.) Screen size 600 x 600
+2.) Draw four 30px wide side rails on all four sides of the window
+3.) Make each side rail a different color.
+4.) Draw 30 boxes(squares) of random size from 10-50 pixels
+5.) Animate them starting at random speeds from -300 to +300 pixels/second. 
+6.) All boxes must be moving.
+7.) Start all boxes in random positions between the rails.
+8.) Bounce boxes off of the side rails when the box edge hits the side rail.
+9.) When the box bounces change its color to the rail it just hit.
+10.)Title the window 30 Boxes
+'''
