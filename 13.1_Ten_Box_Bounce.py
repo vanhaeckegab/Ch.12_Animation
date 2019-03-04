ff6b4fd299862d3c8c495400430e647d2ce669cf
Marc Hermon <hermonm@urbandaleschools.com>
2019-03-03 18:33:06 -0600

    Create 13.1_Ten_Box_Bounce.py

---
+++ 13.1_Ten_Box_Bounce.py
@@ -0,0 +1,13 @@
+'''
+10 BOX BOUNCE PROGRAM
+--------------------
+
+Modify the Ball Bounce Program to create the following:
+1.) Screen size 600 x 600
+2.) Draw four 30px wide side rails on all four sides of the window
+3.) Make each side rail a different color.
+4.) Draw 10 boxes(squares) of random size from 10-50 pixels
+5.) Animate them at different speeds from 120-300 pixels/second
+6.) Bounce boxes off of the side rails when the box edge hits the side rail.
+7.) When the box bounces change its color to the rail it just bounced off of.
+'''
