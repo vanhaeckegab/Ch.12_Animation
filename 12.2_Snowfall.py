2217df30e47e97e12b9a5f26acb04b336006671d
Marc Hermon <hermonm@urbandaleschools.com>
2019-03-04 16:47:10 -0600

    Update 12.2_Snowfall.py

--- 12.2_Snowfall.py
+++ 12.2_Snowfall.py
@@ -12,7 +12,7 @@ the following requirements:
 6.) Random downward speed of -4 to -1
 7.) Start snowflakes again at random x from 0-600 and random y from 600-700
 8.) Generate 300 snowflakes
-9.) Color snowflake #1 red
+9.) Color snowflake #1 red just for fun.
 10.) All other snowflakes should be white.
 
 
