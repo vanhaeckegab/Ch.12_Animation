6f5c488650c814156af0e27177dffae7c2ce4076
Marc Hermon <hermonm@urbandaleschools.com>
2019-03-03 19:41:42 -0600

    Create 13.2_Snowfall.py

---
+++ 13.2_Snowfall.py
@@ -0,0 +1,9 @@
+'''
+SNOWFALL
+--------
+We are going to redo the Snowfall program we did in Chapter 11
+now that we have learned about classes. Create a 600 x 600 window
+and make it snow. Use random speeds, starting positions and re-starting
+positions above the window.
+
+'''
