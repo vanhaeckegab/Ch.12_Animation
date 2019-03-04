1ef9979dc6c4432eb341ab99fdfe0ec057b1988e
Marc Hermon <hermonm@urbandaleschools.com>
2019-03-03 21:26:01 -0600

    Update 13.2_Snowfall.py

--- 13.2_Snowfall.py
+++ 13.2_Snowfall.py
@@ -2,8 +2,18 @@
 SNOWFALL
 --------
 We are going to redo the Snowfall program we did in Chapter 11
-now that we have learned about classes. Create a 600 x 600 window
-and make it snow. Use random speeds, starting positions and re-starting
-positions above the window.
+now that we have learned about classes. Meet the following requirements:
+
+1.) Create a 600 x 600 window with black background
+2.) Window title equals "Snowfall"
+3.) Crossbars 10 px wide. Snow must be outside!
+4.) Make snowflake radius random between 1-3
+5.) Randomly start snowflakes anywhere in the window.
+6.) Random downward speed of -4 to -1
+7.) Start snowflakes again at random x from 0-600 and random y from 600-700
+8.) Generate 300 snowflakes
+9.) Color snowflake #1 red
+10.) All other snowflakes should be white.
+
 
 '''
