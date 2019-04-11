4de0894b95cbfedb974e8f4fbcb2e567fede74ab
Marc Hermon <hermonm@urbandaleschools.com>
2019-04-10 22:09:57 -0500

    Update 12.1_Thirty_Box_Bounce.py

--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -7,7 +7,7 @@ Ball Bounce Program to create the following:
 1.) Screen size 600 x 600
 2.) Draw four 30px wide side rails on all four sides of the window
 3.) Make each side rail a different color.
-4.) Draw 30 boxes(squares) of random size from 10-50 pixels
+4.) Draw 30 black boxes(squares) of random size from 10-50 pixels
 5.) Animate them starting at random speeds from -300 to +300 pixels/second. 
 6.) All boxes must be moving.
 7.) Start all boxes in random positions between the rails.
@@ -19,5 +19,6 @@ Helpful Hints:
 1.) When you initialize the MyGame class create an empty list called self.boxlist=[] to hold all of your boxes.
 2.) Then use a for i in range(30): list to instantiate boxes and append them to the list.
 3.) In the on_draw section use: for box in self.boxlist: box.draw_box()
-4.) In the on_update section use: for box in self.boxlist: box.update_box()
+4.) Also in the on_draw section draw the side rails.
+5.) In the on_update section use: for box in self.boxlist: box.update_box()
 '''
