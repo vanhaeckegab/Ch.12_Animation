3e5ae47e44fe79292f468ecdd5dc93073ae5dbbd
vanhaeckegab <vanhaeckegab@urbandaleschools.com>
2022-02-16 12:16:00 -0600

    ch09

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -8,17 +8,17 @@ All questions are about the final code in Chapter 12:
 2.) What are the variables dx and dy?
 delta x and delta y
 3.) How many pixels/sec does the ball move in the x-direction?
-1
+3
 4.) How many pixels/sec does the ball move in the y-direction?
-1
+2
 5.) Which method is run 60 times/second?
 on_update
 6.) What does this code do?   self.dx *= -1
-bounces the ball off of the wall
+makes the x go in the opposite direction
 7.) What does this code do?  self.pos_y += self.dy
-moves the ball up equal to delta why
+Changes the y of the ball
 8.) What is the width of the window?
-SW 640
+SW = 640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
 if the ball has hit the ceiling
 10.) What is this code checking? if self.pos_x < self.rad
